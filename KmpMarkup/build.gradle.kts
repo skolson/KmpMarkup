36b9a8f29b4428e8c992dd33f6894eccf0be219b
steve-olson <skolson5903@gmail.com>
2025-08-18 01:39:07 -0700

    Add publishing config

--- KmpMarkup/build.gradle.kts
+++ KmpMarkup/build.gradle.kts
@@ -1,16 +1,24 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinMultiplatform
+
 plugins {
     libs.plugins.also {
         alias(it.kotlin.multiplatform)
         alias(it.android.library)
+        alias(it.dokka.base)
+        alias(it.maven.publish.vannik)
     }
 }
 val appVersion = libs.versions.appVersion.get()
 group = libs.versions.appId.get()
 version = libs.versions.appVersionName.get()
+val appleFrameworkName = "KmpMarkup"
+val publishDomain = "io.github.skolson"
+val githubUri = "skolson/$appleFrameworkName"
+val githubUrl = "https://github.com/$githubUri"
 
-group = "com.oldguy"
+group = publishDomain
 version = appVersion
-val moduleName = "KmpMarkup"
 
 val javaLanguageVersion = JavaLanguageVersion.of(libs.versions.javaVersion.get().toInt())
 val javaVersion = JavaVersion.toVersion(libs.versions.javaVersion.get())
@@ -48,21 +56,21 @@ kotlin {
     iosArm64 {
         binaries {
             framework {
-                baseName = moduleName
+                baseName = appleFrameworkName
             }
         }
     }
     iosX64 {
         binaries {
             framework {
-                baseName = moduleName
+                baseName = appleFrameworkName
             }
         }
     }
     iosSimulatorArm64 {
         binaries {
             framework {
-                baseName = moduleName
+                baseName = appleFrameworkName
             }
         }
     }
@@ -104,6 +112,44 @@ kotlin {
             languageSettings {
                 optIn("kotlin.ExperimentalUnsignedTypes")
             }
+        }
+    }
+}
+
+mavenPublishing {
+    coordinates(publishDomain, name, appVersion)
+    configure(
+        KotlinMultiplatform(
+            JavadocJar.Dokka("dokkaGeneratePublicationHtml"),
+            true,
+            listOf("debug", "release")
+        )
+    )
+
+    pom {
+        name.set("Kotlin Multiplatform XML Parser")
+        description.set("Library for simple XML parsing on supported 64 bit platforms; Android, IOS, Windows, Linux, MacOS")
+        url.set(githubUrl)
+        inceptionYear.set("2025")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("oldguy")
+                name.set("Steve Olson")
+                email.set("skolson5903@gmail.com")
+                url.set("https://github.com/skolson/")
+            }
+        }
+        scm {
+            url.set(githubUrl)
+            connection.set("scm:git:git://git@github.com:${githubUri}.git")
+            developerConnection.set("scm:git:ssh://git@github.com:${githubUri}.git")
         }
     }
 }
